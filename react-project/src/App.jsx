4be77ae11edbf2b0961f323ef72770b7be8cc50b
plamin <ph856776@gmail.com>
2025-08-28 22:41:29 -0300

    sadsadas

---
+++ react-project/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Header 
+      titulo="Gui Negão"
+      texto="9 do hexa"
+      cor="red"/>
+    <Header 
+      titulo="Matheuzinho"
+      texto="Lateral do hexa"
+      cor="blue"/>
+    <Header 
+      titulo="Hugo Souza"
+      texto="Goleiro do hexa"
+      cor="black"/>
+    </>
+  )
+}
+
+export default App
---
+++ react-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(props){
+    return (
+    <div style={{color: props.cor}}>
+        <h1>{props.titulo}</h1>
+        <p>{props.texto}</p>
+    </div>
+    )
+}
+
+export default Header
