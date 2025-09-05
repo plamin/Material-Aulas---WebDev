f93fa1c5f07549a90779028d4e345271fef6db48
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-04 22:43:21 -0300

    Pedro

---
+++ Aula 5 - Imagens e Rotas/react-tailwind/src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import AdminLayout from "../pages/AdminLayout";
+import Usuarios from "../pages/Usuarios";
+import Dashboard from "../pages/Dashboard";
+import PageNotFound from "../pages/PageNotFound"
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AdminLayout/>,
+        errorElement: <PageNotFound/>,
+        children: [
+            {
+                index: true,
+                element: <Dashboard/>
+            },
+            {
+                path: "/usuarios",
+                element: <Usuarios/>
+            },
+        ]
+    },
+])
